06cd5e1b5fa07e6f700ecec65656e35b6c18821a
tommarques56 <36605788+tommarques56@users.noreply.github.com>
2020-02-04 00:18:47 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -298,7 +298,7 @@ def creat(nb):
             sleep(2)
             e = driver.find_element_by_class_name("slider-counter-current").text
             s=e.split(':')
-            x=int(s[0])
+            x=int(s[1])
             print(x)
         
 
