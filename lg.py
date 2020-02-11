34288eff6d475e08e6a3f1767359eb5a7e73140d
tommarques56 <36605788+tommarques56@users.noreply.github.com>
2020-02-11 21:47:26 +0100

    Add files via upload

--- lg.py
+++ lg.py
@@ -174,8 +174,8 @@ def launch(driver,em,md):
     driver.refresh()
     # sleep(100)
     driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/button[1]').click()
-    driver.find_element_by_id('login_mail').send_keys(em)
-    driver.find_element_by_id('login_password').send_keys(md)
+    driver.find_element_by_id('login_mail').send_keys("spotify5@coteenfants.f")
+    driver.find_element_by_id('login_password').send_keys("zezette")
     WebDriverWait(driver, 300).until(lambda x: x.find_element_by_css_selector('.antigate_solver.solved'))    
         
     # driver.find_element_by_class_name('unlogged-btn-label').click()
