edb844e6b0ff7c557aa85aaab9de1fe53aac0300
tommarques56 <36605788+tommarques56@users.noreply.github.com>
2020-02-16 13:52:45 +0100

    Add files via upload

--- lg.py
+++ lg.py
@@ -142,8 +142,11 @@ def file_lengthy(fname):
 
     
 def count(driver):
-    
-    f = open("/root/login.txt","rw")
+    try:
+        f.close()
+    except:
+        True
+    f = open("/root/login.txt","r+")
     num_lines = sum(1 for line in f)
     N = random.randrange(0,251)
     print(N)
