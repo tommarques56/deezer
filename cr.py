aebdd4584cb30fae59d45da8cfc5e4a5f71e8c57
tommarques56 <36605788+tommarques56@users.noreply.github.com>
2020-03-20 15:25:47 +0100

    Add files via upload

--- cr.py
+++ cr.py
@@ -265,7 +265,7 @@ def launch(driver):
     driver.get("https://www.deezer.com/register")
     try:
         
-        WebDriverWait(driver, 1).until(lambda x: x.find_element_by_class_name('logo-deezer'))  
+        WebDriverWait(driver, 10).until(lambda x: x.find_element_by_class_name('logo-deezer'))  
         
     except:
         driver.quit()
